b37d266e41b6090b88ab1644f3a3af1ff5a30209
abdurraihan <abdurraihan898@gmail.com>
2023-01-21 22:55:36 +0600

    after complite hotels routes crud

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import usersRoute from "./routes/users.js"
 
 const app = express();
 
+
+
 dotenv.config();
 
 const connect = async () =>{
@@ -27,12 +29,30 @@ mongoose.connection.on('disconnected', () => {
 //     console.log("mongodb connected");
 //   });
 
+app.get('/',(req,res)=>{
+    res.send("Best In hotel booking server is running");
+})
+
 // middleware 
+app.use(express.json());
 
 app.use("/api/auth",authRoute);
 app.use("/api/hotels",hotelsRoute);
 app.use("/api/rooms",roomsRoute);
 app.use("/api/users",usersRoute);
+
+// error handling 
+app.use((err,req,res,next)=>{
+  const errorStatus = err.status || 500 ;
+  const errorMessage = err.errorMessage || "something wrong "
+  return res.status(errorStatus).json({
+    success:false,
+    status:errorStatus,
+    message:errorMessage,
+    stack: err.stack
+
+  })
+})
 
 
 app.listen(7000, ()=>{
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,15 +1,25 @@
 import express from  "express"
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/controller.hotel.js";
+
 
 const router = express.Router();
 
-router.post("/", async(req,res)=>{
-    try{
+// CREATE 
+router.post("/", createHotel)
+
+//UPDATE
+router.put("/:id",updateHotel)
+
+//DELETE
+router.delete("/:id", deleteHotel)
+
+//GET by Id
+router.get("/:id", getHotel)
+
+//GET ALL 
+router.get("/", getHotels)
 
 
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
 
 
 export default router;
